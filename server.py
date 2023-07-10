ac4605561bfde03a793a7751ee02a9678d79eb68
Dio Lemos <pedro.devsc@outlook.com>
2023-07-10 06:06:33 -0300

    first commit

---
+++ server.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+#hey, flask. I will be using sqlite
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///books.db"
+
+db = SQLAlchemy()
+
+db.init_app(app)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db
+    
+with app.app_context():
+    db.create_all()
+    
+@app.route('/')
+def home():
+    return '<h1>Welcome</h1>'    
+
+
+if __name__ == "__main__":
+    app.run(debug=True)    
